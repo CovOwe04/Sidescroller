0365a2f5fb94cc0070ee8a426047b5f2b17532a7
Owen Covach <owenacov@gmail.com>
2022-02-26 15:53:37 -0500

    main changes

--- main.py
+++ main.py
@@ -19,13 +19,107 @@ lava_img = pygame.transform.scale(pygame.image.load('Assets/lavaMid.png'), (100,
 
 bgX = 0
 bgX2 = bg_img.get_width()
+game_over = 0
 
 def redrawWindow(win):
     win.blit(bg_img, (bgX, 0))
     win.blit(bg_img, (bgX2,0))
     world.draw()
+    player.update(game_over)
+
+class Player():
+    def __init__(self, x, y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        self.jumped = False
+
+        img_jump = pygame.image.load('Assets/p1_jump.png').convert_alpha()
+        self.jump_img = pygame.transform.scale(img_jump, (100,160))
+
+        for num in range(1,7):
+            img_right = pygame.image.load(f'Assets/p1_walk{num}.png').convert_alpha()
+            img_right = pygame.transform.scale(img_right, (100,160))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.dead_image = pygame.image.load('Assets/p1_hurt.png').convert_alpha()
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+        self.in_air = True
+        self.sliding = False
+    
+    def update(self, game_over):
+        dx = 0
+        dy = 0
+        walk_cooldown = 3
+        if game_over == 0:
+            #get key presses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+                self.vel_y = -40
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_a]:
+                dx -= 10
+                self.counter +=1
+                self.direction = -1
+            if key[pygame.K_d]:
+                dx += 10
+                self.counter +=1
+                self.direction = 1
+            if key[pygame.K_a] == False and key[pygame.K_d] == False:
+                self.counter = 0
+                self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
 
 
+            #handle animation
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_right[self.index]
+            if self.in_air == True:
+                if self.direction == 1:
+                    self.image = self.jump_img
+                if self.direction == -1:
+                    self.image = self.jump_img 
+
+            #add gravity
+            self.vel_y += 2
+            if self.vel_y > 25:
+                self.vel_y = 25
+            dy += self.vel_y
+             
+            self.rect.x +=dx
+            self.rect.y +=dy
+        
+        elif game_over == -1:
+            if self.direction == 1:
+                self.image = pygame.transform.scale(self.dead_image, (100, 160))
+            if self.direction == -1:
+                self.image = pygame.transform.flip (pygame.transform.scale(self.dead_image, (100, 160)), True, False)
+        
+        win.blit(self.image, self.rect)
+        
+        return game_over
 
 class World():
     def __init__(self, data):
@@ -61,10 +155,9 @@ world_data =[
 [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0],
 [1 ,1 ,2 ,2 ,1 ,1 ,1 ,2 ,2 ,1 ,1 ,1 ,2 ,2 ,1 ,1],
 ]
- 
-
 
 world = World(world_data)
+player = Player(100, 500)
 
 run = True
 
