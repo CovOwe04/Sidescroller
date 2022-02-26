a42e1cea20205b72649aefecb04fe6b26cb01855
Owen Covach <owenacov@gmail.com>
2022-02-26 14:28:52 -0500

    Rendered Background

---
+++ README.txt
@@ -0,0 +1,14 @@
+Steps to make Sidescroller:
+
+1. render background (Done)
+2. render single block
+3. create data set to create 1 frame of world
+4. render world
+5. create player
+6. render player
+7. player movement (left,right, and collisions included)
+8. create moving world
+9. create world generation
+10. create score system (1 health death)
+11. create enemies
+12. fix bugs/make improvements
--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("hello world")
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+width,height = 1600, 1000
+
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption('HumansFall')
+
+bg_img = pygame.image.load('Assets/bg_castle.png').convert_alpha()
+
+bgX = 0
+bgX2 = bg_img.get_width()
+
+def redrawWindow(win):
+    win.blit(bg_img, (bgX, 0))
+    win.blit(bg_img, (bgX2,0))
+
+
+ 
+run = True
+
+while run:
+
+    bgX -= 1
+    bgX2 -= 1
+
+    if bgX < bg_img.get_width() * -1:
+        bgX = bg_img.get_width()
+    if bgX2 < bg_img.get_width() * -1:
+        bgX2 = bg_img.get_width()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+
+    redrawWindow(win)
+    pygame.display.update()
+    clock.tick(fps)
+    
